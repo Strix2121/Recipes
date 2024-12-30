828ffa6d7747a12a548c8bab38f5643c67d61b6a
Strix2121 <mstar661@gmail.com>
2024-12-30 14:23:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-document.getElementById('showRecipe').addEventListener('click', function() {
-    const recipeName = document.getElementById('recipeName').value;
-    if (recipeName === '') {
-        alert('يرجى ملء الحقل الخاص باسم الوصفة');
-    } else {
-        document.getElementById('displayRecipeName').innerText = recipeName;
-    }
-});
-
-document.getElementById('addIngredient').addEventListener('click', function() {
-    const ingredient = document.getElementById('ingredient').value;
-    if (ingredient === '') {
-        alert('يرجى ملء الحقل الخاص بالمكونات');
-    } else {
-        const li = document.createElement('li');
-        li.innerText = ingredient;
-        document.getElementById('ingredientList').appendChild(li);
-        document.getElementById('ingredient').value = '';
-    }
-});
-
-document.getElementById('clearAll').addEventListener('click', function() {
-    document.getElementById('recipeName').value = '';
-    document.getElementById('ingredient').value = '';
-    document.getElementById('displayRecipeName').innerText = '';
-    document.getElementById('ingredientList').innerHTML = '';
-});
-
 document.getElementById('toggleLanguage').addEventListener('click', function() {
     const langBtn = document.getElementById('toggleLanguage');
     const pageTitle = document.getElementById('pageTitle');
@@ -34,6 +6,7 @@ document.getElementById('toggleLanguage').addEventListener('click', function() {
     const clearAll = document.getElementById('clearAll');
     const recipeNameInput = document.getElementById('recipeName');
     const ingredientInput = document.getElementById('ingredient');
+    const recipeTableHeaders = document.querySelectorAll('#recipeTable th');
     
     if (langBtn.innerText === 'Switch to English') {
         langBtn.innerText = 'التبديل إلى العربية';
@@ -43,13 +16,17 @@ document.getElementById('toggleLanguage').addEventListener('click', function() {
         clearAll.innerText = 'Clear All';
         recipeNameInput.placeholder = 'Enter Recipe Name';
         ingredientInput.placeholder = 'Enter Ingredient';
+        recipeTableHeaders[0].innerText = 'Recipe Name';
+        recipeTableHeaders[1].innerText = 'Ingredients';
     } else {
         langBtn.innerText = 'Switch to English';
-        pageTitle.innerText = 'منشئ الوصفات';
+        pageTitle.innerText = 'الوصفات';
         showRecipe.innerText = 'عرض الوصفة';
         addIngredient.innerText = 'إضافة المكونات';
         clearAll.innerText = 'مسح الكل';
         recipeNameInput.placeholder = 'ادخل اسم الوصفة';
         ingredientInput.placeholder = 'ادخل المكونات';
+        recipeTableHeaders[0].innerText = 'اسم الوصفة';
+        recipeTableHeaders[1].innerText = 'المكونات';
     }
 });
